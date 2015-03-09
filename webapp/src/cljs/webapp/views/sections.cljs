bbccde39847813df6715f4e188b51196edbffeb5
martin.haesler <martin.haesler@gmail.com>
2015-03-09 18:31:16 +0000

    finish personal-project-element

--- webapp/src/cljs/webapp/views/sections.cljs
+++ webapp/src/cljs/webapp/views/sections.cljs
@@ -16,7 +16,7 @@
    [:h3 "About"]
    [:p "Martin Haesler is a former sales man for plumbing and heating projects, who after 17 years of working in this line of work, decided it was time for a change, see something of the world, and try for a completely different career path. Having always been interested in computer technology, an avid Linux user for many years, he decided on programming as a new career. Martin Haesler is quick to learn new things, self motivated and eager to improve."]])
 
-(def personal-projects
+(defn personal-projects []
   [
    {:name "MetalHead"
     :url  "https://github.com/mswift42/MetalHead"
@@ -30,3 +30,17 @@
     :url "https://github.com/mswift42/go-beebster"
     :pic "images/pic1.png"
     :summary "go-beebster is a Web Gui frontend for <a href='https://github.com/get-iplayer/get-iplayer'>get-iplayer</a>. You can search for Iplayer programmes, either by name or category, see their description and download the programme. Beebster is implemented in Go, running the command line application get-iplayer in the background to search for the desired programme and info using regular expressions. It displays the results in the browser, showing a thumbnail and the programme's title. Downloading opens a gnome-terminal running the get-iplayer download command."}])
+
+(defn personalproject-element []
+  [:div.container
+   [:h3 "Personal Projects"]
+   (for [i (personal-projects)]
+     [:div.persprojelement
+      [:div.projheader
+       [:h4 (:name i)]]
+      [:div.projurl
+       [:a {:href (:url i)} "github url"]]
+      [:div.projimg
+       [:img {:src (:pic i)}]]
+      [:div.projsummary
+       [:p (:summary i)]]])])      
